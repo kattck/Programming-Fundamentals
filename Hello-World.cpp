0c253ecf69f9ed890e3fe61ed2bc9b284106ed9e
Cam Stephens <kattck@gmail.com>
2017-03-15 20:05:50 -0400

    new file:   Hello-World.cpp

---
+++ Hello-World.cpp
@@ -0,0 +1,50 @@
+// Mod 01.cpp : Defines the entry point for the console application.
+//Cam Stephens
+// 07/10/2015
+//Mod 01 Exercise
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+
+//main
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+        //Display Message
+        cout << "Hello World" << endl;
+
+
+        system("pause");
+
+        cout << "HHH      hhh     eeeeeeeeeee    lllll         lllll           ooooooooooooo" << endl;
+        cout << "hhh      hhh     eeeeeeeeeee    lllll         lllll          ooooooooooooooo" << endl;
+        cout << "hhh      hhh     eee            lllll         lllll         oooo         oooo" << endl;
+        cout << "hhh      hhh     eee            lllll         lllll         oooo         oooo" << endl;
+        cout << "hhhhhhhhhhhh     eeeeee         lllll         lllll         oooo         oooo" << endl;
+        cout << "hhhhhhhhhhhh     eeeeee         lllll         lllll         oooo         oooo" << endl;
+        cout << "hhh      hhh     eee            lllll         lllll         oooo         oooo" << endl;
+        cout << "hhh      hhh     eee            lllll         lllll         oooo         oooo" << endl;
+        cout << "hhh      hhh     eeeeeeeeeee    lllllllllll   lllllllllll    oooo       oooo" << endl;
+        cout << "hhh      hhh     eeeeeeeeeee    lllllllllll   lllllllllll      ooooooooooo" << endl;
+
+
+
+
+        cout << "wwwwwww            wwwwww    ooooooooooooo     rrrrrrrrrr   lllll     ddddddd" << endl;
+        cout << "wwwwwww            wwwwww    oooooooooooooo    rrrr    rrr  lllll     dddd  ddd" << endl;
+        cout << "wwwwwww            wwwwww    oooo       oooo   rrrr     rr  lllll     dddd   dd" << endl;
+        cout << "wwwwwww    wwww    wwwwww    oooo       oooo   rrrr   rrr   lllll     dddd    dd" << endl;
+        cout << "wwwwwww    wwww    wwwwww    oooo       oooo   rrrr  rrr    lllll     dddd    dd" << endl;
+        cout << "wwwwwww    wwww    wwwwww    oooo       oooo   rrrr   rrr   lllll     dddd    dd" << endl;
+        cout << "wwwwwww    wwww    wwwwww    oooo       oooo   rrrr    rrr  lllllllllldddd   dd" << endl;
+        cout << "wwwwwwwwwwwwwwwwwwwwwwwww     oooooooooooo     rrrr     rrr lllllllllldddd dd" << endl;
+
+        system("pause");
+
+        return 0;
+
+}
